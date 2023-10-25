0651e8ca3def53e0d9b5648bcf5f4ff391e7c88a
Lautaro Pinto <lautaro.pinto@y.uno>
2023-10-25 15:24:41 -0300

    first commit

---
+++ TestCyber/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TestCyber
+//
+//  Created by Viviana Amezquita on 24/08/23.
+//
+
+import UIKit
+import CardinalMobile
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
